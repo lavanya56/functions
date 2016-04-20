e36e6d2b69846869f3e2c0d22adb9dccafedfef1
morsh <mor.shemesh@gmail.com>
2016-04-20 18:17:16 +0300

    adding parallel async functions

--- FunkTheFunc/index.js
+++ FunkTheFunc/index.js
@@ -1,3 +1,5 @@
+var async = require('async');
+
 module.exports = function (context, myTimer) {
     var timeStamp = new Date().toISOString();
     
@@ -7,5 +9,16 @@ module.exports = function (context, myTimer) {
     }
     context.log('Node.js timer trigger function ran!', timeStamp);   
     
-    context.done();
+    async.parallel([
+        function (cb) {
+            console.log('wow');
+        },
+        function (cb) {
+            console.log('zee');
+        }
+    ],
+    function () {
+        console.info('over and out');
+        context.done();
+    });
 };
