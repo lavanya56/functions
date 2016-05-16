d91dde3f0cbffdab7eab8a5e8fe7f3c4fb87d95b
morsh <mor.shemesh@gmail.com>
2016-05-16 18:08:37 +0300

    remove autocollect

--- FunkTheFunc/index.js
+++ FunkTheFunc/index.js
@@ -4,7 +4,7 @@ var appInsights = require("applicationinsights");
 
 //appInsights.setup("jennn75xcjm11rhjdi068xa04tqs8ymxeffk3kgm").start();
 //var client = appInsights.getClient("jennn75xcjm11rhjdi068xa04tqs8ymxeffk3kgm");
-appInsights.setup("54d915d2-c67d-4d23-8de1-7f50a99251a6").setAutoCollectConsole(true).start();
+appInsights.setup("54d915d2-c67d-4d23-8de1-7f50a99251a6").start();
 var client = appInsights.getClient("54d915d2-c67d-4d23-8de1-7f50a99251a6");
 
 function formatArgs(args){
